9f35722b4125caea6926e7336446070cad460aa1
Helen <le.helen19@gmail.com>
2022-01-19 15:56:10 -0800

    Added log in forms and validators

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route("/")
 @app.route("/home")
@@ -16,3 +17,12 @@ def home():
         }
     ]
     return render_template("home.html", title="Home", user=user, posts=posts)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login requested for user {}, remember_me={}".format(
+            form.username.data, form.remember_me.data))
+        return redirect("/home")
+    return render_template("login.html", title="Sign In", form=form)
