c8885397e9ef6b87ea84f35d9cbc64421475aebe
Helen <le.helen19@gmail.com>
2022-01-20 18:32:21 -0800

    Set up sqlite database and alembic migration repo

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 import os
+from pickle import FALSE
 from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 load_dotenv()
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    # Flask-SQLAlchemy extension takes location of database, our base directory if url unprovided
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
