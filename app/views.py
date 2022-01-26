71e32abec0e603c8e5e9ec1ac0734ef29e9dc914
Helen <le.helen19@gmail.com>
2022-01-26 15:49:51 -0800

    Added login requirement for views

--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 from app import app
 from app.forms import LoginForm
 from app.models import User
 
 @app.route("/")
 @app.route("/home")
+@login_required
 def home():
     user = {"username": "Helen"}
     posts = [
@@ -33,7 +35,10 @@ def login():
             flash("Invalid username or password")
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for("home"))
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            redirect(url_for("home"))
+        return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
 
 @app.route("/logout")
