60c23d17f63957eeb1510e0ddb524adf49d4bd1d
Helen <le.helen19@gmail.com>
2022-01-28 14:09:38 -0800

    flask-bootstrap facelift

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +13,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
+bootstrap = Bootstrap(app)
 
 from app import views, models, errors
 
