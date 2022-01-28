4e727b677dee1dafefa229949e242819c6bcbadc
Helen <le.helen19@gmail.com>
2022-01-28 13:43:12 -0800

    Added pagination support, config set to 3 pages

--- config.py
+++ config.py
@@ -17,3 +17,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     ADMINS = ["le.helen19@gmail.com"]
+    POSTS_PER_PAGE = 3
