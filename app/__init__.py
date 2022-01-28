bc430ef9b724cc7ed4575b402e912aab65c99ace
Helen <le.helen19@gmail.com>
2022-01-28 15:50:23 -0800

    Localized time zomes with moment.js (flask-moment)

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
